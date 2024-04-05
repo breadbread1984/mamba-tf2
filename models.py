78c686e9ca0861934477c3b54c1b94d3357b5203
breadbread1984 <breadbread1984@163.com>
2024-04-05 17:17:19 +0800

    update

--- models.py
+++ models.py
@@ -24,10 +24,9 @@ class RMSNorm(tf.keras.layers.Layer):
     return cls(**config)
 
 class SSM(tf.keras.layers.Layer):
-  def __init__(self, d_model, seq_len, expand = 2, d_state = 16, bias = False):
+  def __init__(self, d_model, expand = 2, d_state = 16, bias = False):
     super(SSM, self).__init__()
     self.d_model = d_model
-    self.seq_len = seq_len
     self.expand = expand
     self.d_state = d_state
     self.bias = bias
@@ -62,7 +61,7 @@ class SSM(tf.keras.layers.Layer):
     deltaB_x = tf.expand_dims(delta, axis = -1) * tf.expand_dims(B, axis = -2) * tf.expand_dims(x, axis = -1) # deltaB_x.shape = (batch, seq_len, expand * d_model, d_state)
     state = tf.zeros((tf.shape(x)[0], self.expand * self.d_model, self.d_state)) # state.shape = (batch, expand * d_model, d_state)
     ys = list()
-    for i in range(self.seq_len): # loop over seq_len
+    for i in range(tf.shape(x)[1]): # loop over seq_len
       state = deltaA[:, i] * state + deltaB_x[:, i] # state.shape = (batch, expand * d_model, d_state)
       y = tf.math.reduce_sum(state * tf.expand_dims(C[:, i], axis = 1), axis = -1) # y.shape = (batch, expand * d_model)
       ys.append(y)
@@ -72,7 +71,6 @@ class SSM(tf.keras.layers.Layer):
   def get_config(self):
     config = super(SSM, self).get_config()
     config['d_model'] = self.d_model
-    config['seq_len'] = self.seq_len
     config['expand'] = self.expand
     config['d_state'] = self.d_state
     config['bias'] = self.bias
@@ -81,20 +79,20 @@ class SSM(tf.keras.layers.Layer):
   def from_config(cls, config):
     return cls(**config)
 
-def MambaBlock(d_model, seq_len, expand = 2, bias = False, d_conv = 4, conv_bias = True, d_state = 16):
-  inputs = tf.keras.Input((seq_len, d_model)) # inputs.shape = (batch, seq_len, d_model)
+def MambaBlock(d_model, expand = 2, bias = False, d_conv = 4, conv_bias = True, d_state = 16):
+  inputs = tf.keras.Input((None, d_model)) # inputs.shape = (batch, seq_len, d_model)
   x_and_res = tf.keras.layers.Dense(2 * expand * d_model, use_bias = bias)(inputs) # results.shape = (batch, seq_len, 2 * expand * d_model)
   x, res = tf.keras.layers.Lambda(lambda x: tf.split(x, 2, axis = -1))(x_and_res) # x.shape = (batch, seq_len, expand * d_model)
   # channel mixing
   x = tf.keras.layers.Conv1D(expand * d_model, kernel_size = (d_conv,), padding = 'same', use_bias = conv_bias, groups = expand * d_model, activation = tf.keras.activations.silu)(x) # x.shape = (batch, seq_len, expand * d_model)
   # spatial mixing
-  y = SSM(d_model, seq_len, expand, d_state, bias)(x) # y.shape = (batch, seq_len, d_model * expand)
+  y = SSM(d_model, expand, d_state, bias)(x) # y.shape = (batch, seq_len, d_model * expand)
   y = tf.keras.layers.Lambda(lambda x: x[0] * tf.nn.silu(x[1]))([y, res])
   outputs = tf.keras.layers.Dense(d_model, use_bias = bias)(y)
   return tf.keras.Model(inputs = inputs, outputs = outputs)
 
 if __name__ == "__main__":
-  block = MambaBlock(256, 10)
+  block = MambaBlock(256)
   inputs = np.random.normal(size = (4, 10, 256))
   outputs = block(inputs)
   print(outputs.shape)
